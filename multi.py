ae48e4e45fa1ac461a0ff5d7850ab7a6c41e3976
SatoshiShimada <mylinux1204@gmail.com>
2015-10-29 00:47:12 +0900

    comment-out to load training data and test data

--- multi.py
+++ multi.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python2
 # coding: utf-8
 
+# import library
+import sys
+import numpy as np
+# import my library
 import network
 import mnist_loader_with_pickle
-import numpy as np
 import read
-import sys
 
 if __name__ == '__main__':
-    training_data, validation_data, test_data = \
-        mnist_loader_with_pickle.load_data_wrapper()
+    #training_data, validation_data, test_data = \
+    #    mnist_loader_with_pickle.load_data_wrapper()
     net = network.Network([784, 30, 10])
     #net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
     img = read.load_image(sys.argv[1], int(sys.argv[2]))
