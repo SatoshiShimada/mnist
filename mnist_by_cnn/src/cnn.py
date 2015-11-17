4c94f47d370997b0fa4ef3cc7ae5a3d0d9861c94
SatoshiShimada <mylinux1204@gmail.com>
2015-11-18 02:44:33 +0900

    Modified cnn.py

--- mnist_by_cnn/src/cnn.py
+++ mnist_by_cnn/src/cnn.py
@@ -6,6 +6,13 @@ import mnist_loader_with_pickle
 
 import network
 
+### Process flow ###
+## convolution layer
+## pooling layer
+## convolution layer
+## pooling layer
+## fully-connected layer
+
 class CNN():
     def __init__(self, flt, image_size, output=False):
         """
@@ -31,70 +38,73 @@ class CNN():
         Convolution and Pooling
         Add biases, activation function
         """
-        #data = training_data[0][0].reshape((self.image_width, self.image_height))
         for train in training_data:
             data = train[0].reshape((self.image_width, self.image_height))
 
-            ## Convolution
-            fmapsize_width = self.image_width - 2 * int(self.filter_size / 2)
-            fmapsize_height = self.image_height - 2 * int(self.filter_size / 2)
-            filtered_data = []
-            for filters in self.w:
-                for filter_ in filters:
-                    dat = []
-                    for n1 in xrange(fmapsize_height):
-                        for n2 in xrange(fmapsize_width):
-                            buf = [x[n2: n2+self.filter_size] for x in data[n1: n1+self.filter_size]]
-                            dat.append(np.dot(np.array(buf), filter_).sum())
-                    filtered_data.append(dat)
+    def convolution(self, x):
+        ## Convolution
+        fmapsize_width = self.image_width - 2 * int(self.filter_size / 2)
+        fmapsize_height = self.image_height - 2 * int(self.filter_size / 2)
+        filtered_data = []
+        for filters in self.w:
+            for filter_ in filters:
+                dat = []
+                for n1 in xrange(fmapsize_height):
+                    for n2 in xrange(fmapsize_width):
+                        buf = [x[n2: n2+self.filter_size] for x in data[n1: n1+self.filter_size]]
+                        dat.append(np.dot(np.array(buf), filter_).sum())
+                filtered_data.append(dat)
 
-            ## feature maps (filter_count)
-            feature_maps = np.array(filtered_data)
-            if self.parameter_output == True:
-                np.savetxt('../parameter/out1.csv', feature_maps, delimiter=',')
+        ## feature maps (filter_count)
+        feature_maps = np.array(filtered_data)
+        if self.parameter_output == True:
+            np.savetxt('../parameter/out1.csv', feature_maps, delimiter=',')
 
 #####################################################################
-# TODO: sepalate convolution, pooling, biases and activation function
+# TODO: separate convolution, pooling, biases and activation function
 #####################################################################
-            ## Pooling
-            ## Max-pooling
-            ## Window size: 2x2
-            fmap_buf = []
-            for data in feature_maps:
-                fmap_out = []
-                fmap = data.reshape((fmapsize_width, fmapsize_height))
-                for y in xrange(fmapsize_height / 2):
-                    y *= 2
-                    buf1, buf2 = fmap[y:y+2]
-                    buf = []
-                    count = 0
-                    for a in zip(buf1, buf2):
-                        buf.append(a[0])
-                        buf.append(a[1])
-                        if (count % 2) == 1:
-                            fmap_out.append(max(buf))
-                            buf = []
-                        count += 1
-                fmap_buf.append(fmap_out)
-            feature_maps = np.array(fmap_buf)
-            if self.parameter_output == True:
-                np.savetxt('../parameter/out2.csv', feature_maps, delimiter=',')
-
-            ## Added Biases
-            fmap_buf = []
-            for f, a in zip(feature_maps, self.biases):
+    def pooling(self, x):
+        ## Pooling
+        ## Max-pooling
+        ## Window size: 2x2
+        fmap_buf = []
+        for data in feature_maps:
+            fmap_out = []
+            fmap = data.reshape((fmapsize_width, fmapsize_height))
+            for y in xrange(fmapsize_height / 2):
+                y *= 2
+                buf1, buf2 = fmap[y:y+2]
                 buf = []
-                [buf.append(x + a) for x in f]
-                fmap_buf.append(buf)
-            feature_maps = np.array(fmap_buf)
-            if self.parameter_output == True:
-                np.savetxt('../parameter/out3.csv', feature_maps, delimiter=',')
+                count = 0
+                for a in zip(buf1, buf2):
+                    buf.append(a[0])
+                    buf.append(a[1])
+                    if (count % 2) == 1:
+                        fmap_out.append(max(buf))
+                        buf = []
+                    count += 1
+            fmap_buf.append(fmap_out)
+        feature_maps = np.array(fmap_buf)
+        if self.parameter_output == True:
+            np.savetxt('../parameter/out2.csv', feature_maps, delimiter=',')
 
-            ## Activation function
-            # sigmoid, tanh, ReLU
-            result = []
-            [result.append(network.sigmoid_vec(x)) for x in feature_maps]
-            feature_maps = np.array(result)
-            if self.parameter_output == True:
-                np.savetxt('../parameter/out4.csv', feature_maps, delimiter=',')
+    def added_biases(self, x):
+        ## Added Biases
+        fmap_buf = []
+        for f, a in zip(feature_maps, self.biases):
+            buf = []
+            [buf.append(x + a) for x in f]
+            fmap_buf.append(buf)
+        feature_maps = np.array(fmap_buf)
+        if self.parameter_output == True:
+            np.savetxt('../parameter/out3.csv', feature_maps, delimiter=',')
+
+    def activation(self, x):
+        ## Activation function
+        # sigmoid, tanh, ReLU
+        result = []
+        [result.append(network.sigmoid_vec(x)) for x in feature_maps]
+        feature_maps = np.array(result)
+        if self.parameter_output == True:
+            np.savetxt('../parameter/out4.csv', feature_maps, delimiter=',')
 
