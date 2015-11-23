837da8dd30f65c4c77730af9c4e97ef2efc39fca
SatoshiShimada <mylinux1204@gmail.com>
2015-11-23 22:08:56 +0900

    Added backpropagation as network3.py

--- mnist_with_backpropagation/test.py
+++ mnist_with_backpropagation/test.py
@@ -1,10 +1,22 @@
-import network
+#!/usr/bin/python2
 
 import numpy as np
 
-logic_and = np.array( \
-    ((0.0, 0.0, 0.0), (0.0, 1.0, 0.0), (1.0, 0.0, 0.0), (1.0, 1.0, 1.0)))
-net = network.Network([2,3,1])
-net.learning(np.array(logic_and))
-
-net.feed_forward(logic_and)
+if False:
+    import network as network
+    logic_and = np.array(((0.0, 0.0, 0.0), (0.0, 1.0, 0.0), (1.0, 0.0, 0.0), (1.0, 1.0, 1.0)))
+    net = network.Network([2,3,1])
+    net.learning(np.array(logic_and))
+    net.feed_forward(logic_and)
+elif False:
+    import network2 as network
+    logic_and = [[np.array([1,2]), 1], [np.array([3,4]), 2]]
+    net = network.Network([2,3,1])
+    net.train(logic_and)
+else:
+    import network3 as network
+    logic_and = [[np.array([0,0]), np.array([0])], [np.array([0,1]), np.array([0])], [np.array([1,0]) ,np.array([0])], [np.array([1,1]), np.array([1])]]
+    logic_and_test = np.array(logic_and)
+    net = network.Network([2,3,1])
+    net.train(logic_and, epoch=500, mini_batch_size=1, learning_rate=0.5)
+    net.feed_forward(logic_and_test)
