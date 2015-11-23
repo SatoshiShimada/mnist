44eccc637a4cc645ac155dff0bcbdc3fb2673544
SatoshiShimada <mylinux1204@gmail.com>
2015-11-23 23:04:34 +0900

    Added mnist dataset

--- mnist_with_backpropagation/test.py
+++ mnist_with_backpropagation/test.py
@@ -3,11 +3,36 @@
 
 import numpy as np
 
-import network3 as network
+import network
 
-logic_and = [[np.array([0,0]), np.array([0])], [np.array([0,1]), np.array([0])], [np.array([1,0]) ,np.array([0])], [np.array([1,1]), np.array([1])]]
-logic_and_test = np.array(logic_and)
-net = network.Network([2,3,1])
-net.train(logic_and, epoch=500, mini_batch_size=1, learning_rate=0.5)
-net.feed_forward(logic_and_test)
+class Logic():
+    logic_and = [[np.array([0,0]), np.array([0])], [np.array([0,1]), np.array([0])], [np.array([1,0]) ,np.array([0])], [np.array([1,1]), np.array([1])]]
+    logic_or = [[np.array([0,0]), np.array([0])], [np.array([0,1]), np.array([1])], [np.array([1,0]) ,np.array([1])], [np.array([1,1]), np.array([1])]]
+    logic_exor = [[np.array([0,0]), np.array([0])], [np.array([0,1]), np.array([1])], [np.array([1,0]) ,np.array([1])], [np.array([1,1]), np.array([0])]]
+
+class MNIST():
+    def __init__(self):
+        import mnist_loader_with_pickle as loader
+        self.training_data, self.validation_data, self.test_data = loader.load_data_wrapper()
+
+if __name__ == '__main__':
+    if True:
+        mnist = MNIST()
+        train_data = mnist.training_data
+        test_data  = mnist.test_data
+        net = network.Network([784, 30, 10])
+        net.train(train_data, epoch=300, mini_batch_size=30, learning_rate=0.4)
+    else:
+        data = Logic()
+        ## create test data
+        logic_and_test  = np.array(data.logic_and)
+        logic_or_test   = np.array(data.logic_or)
+        logic_exor_test = np.array(data.logic_exor)
+
+        train_data = data.logic_or
+        test_data  = logic_or_test
+
+        net = network.Network([2,3,1])
+        net.train(train_data, epoch=300, mini_batch_size=1, learning_rate=0.5)
+        net.feed_forward(test_data)
 
